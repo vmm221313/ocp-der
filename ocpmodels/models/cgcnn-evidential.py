CGCNN-DER normalization issues fixed

ocpmodels/models/cgcnn-evidential.py
@@ -19,6 +19,33 @@ from ocpmodels.common.utils import (
 from ocpmodels.datasets.embeddings import KHOT_EMBEDDINGS, QMOF_KHOT_EMBEDDINGS
 from ocpmodels.models.base import BaseModel
 
+class DenseNormalGamma(nn.Module):
+    def __init__(self, 
+                input_dim, 
+                units, 
+                softplus_beta=1,
+                softplus_threshold=20
+                ):       
+        super(DenseNormalGamma, self).__init__()
+
+        self.units = units
+
+        self.dense = nn.Linear(input_dim, 4 * self.units)
+        self.softplus = torch.nn.Softplus(
+            beta=softplus_beta, threshold=softplus_threshold
+        )
+
+    def evidence(self, x):
+        return self.softplus(x)
+
+    def forward(self, x):
+        output = self.dense(x)
+        mu, logv, logalpha, logbeta = torch.split(output, split_size_or_sections=(1 * self.units), dim=-1)
+        v = self.evidence(logv)
+        alpha = self.evidence(logalpha) + 1
+        beta = self.evidence(logbeta)
+
+        return torch.concat([mu, v, alpha, beta], axis=-1)
 
 @registry.register_model("cgcnn-evidential")
 class CGCNN_Evidential(BaseModel):
@@ -81,10 +108,10 @@ class CGCNN_Evidential(BaseModel):
         self.cutoff = cutoff
         self.otf_graph = otf_graph
 
-        self.units = units
-        self.softplus = torch.nn.Softplus(
-            beta=softplus_beta, threshold=softplus_threshold
-        )
+        # self.units = units
+        # self.softplus = torch.nn.Softplus(
+        #     beta=softplus_beta, threshold=softplus_threshold
+        # )
 
         # Get CGCNN atom embeddings
         if embeddings == "khot":
@@ -121,13 +148,14 @@ class CGCNN_Evidential(BaseModel):
                 layers.append(nn.Linear(fc_feat_size, fc_feat_size))
                 layers.append(nn.Softplus())
             self.fcs = nn.Sequential(*layers)
-        self.fc_out = nn.Linear(fc_feat_size, self.num_targets)
+        # self.fc_out = nn.Linear(fc_feat_size, self.num_targets)
+        self.fc_out = DenseNormalGamma(fc_feat_size, units, softplus_beta, softplus_threshold)
 
         self.cutoff = cutoff
         self.distance_expansion = GaussianSmearing(0.0, cutoff, num_gaussians)
 
-    def evidence(self, x):
-        return self.softplus(x)
+    # def evidence(self, x):
+    #     return self.softplus(x)
 
     @conditional_grad(torch.enable_grad())
     def _forward(self, data):
@@ -171,16 +199,21 @@ class CGCNN_Evidential(BaseModel):
         if hasattr(self, "fcs"):
             mol_feats = self.fcs(mol_feats)
 
-        energy = self.fc_out(mol_feats)
-        mu, logv, logalpha, logbeta = torch.split(
-            energy, split_size_or_sections=(1 * self.units), dim=-1
-        )
+        pred = self.fc_out(mol_feats)
+        # print(pred)
 
-        v = self.evidence(logv)
-        alpha = self.evidence(logalpha) + 1
-        beta = self.evidence(logbeta)
+        # mu, logv, logalpha, logbeta = torch.split(
+        #     energy, split_size_or_sections=(1 * self.units), dim=-1
+        # )
 
-        return torch.concat([mu, v, alpha, beta], axis=-1)
+        # v = self.evidence(logv)
+        # alpha = self.evidence(logalpha) + 1
+        # beta = self.evidence(logbeta)
+
+        # return torch.concat([mu, v, alpha, beta], axis=-1)
+
+        return pred
+
 
     def forward(self, data):
         if self.regress_forces:

ocpmodels/modules/loss.py
@@ -58,10 +58,10 @@ class DDPLoss(nn.Module):
 			loss = self.loss_fn(input, target, natoms)
 		if self.reduction == "mean":
 			num_samples = (
-				batch_size if batch_size is not None else input.shape[0]
+				batch_size if batch_size is not None else input["energy"].shape[0]
 			)
 			num_samples = distutils.all_reduce(
-				num_samples, device=input.device
+				num_samples, device=input["energy"].device
 			)
 			# Multiply by world size since gradients are averaged
 			# across DDP replicas
@@ -74,55 +74,133 @@ class EvidentialLoss(nn.Module):
 		super().__init__()
 		self.lambda_ = lambda_
 
-	def NIG_NLL(self, y, gamma, v, alpha, beta, reduce=True):
-		twoBlambda = 2 * beta * (1 + v)
+	# def NIG_NLL(self, y, gamma, v, alpha, beta, reduce=True):
+	# 	twoBlambda = 2 * beta * (1 + v)
 
-		nll = (
-			0.5 * torch.log(np.pi / v)
-			- alpha * torch.log(twoBlambda)
-			+ (alpha + 0.5) * torch.log(v * (y - gamma) ** 2 + twoBlambda)
-			+ torch.lgamma(alpha)
-			- torch.lgamma(alpha + 0.5)
-		)
+	# 	nll = (
+	# 		0.5 * torch.log(np.pi / v)
+	# 		- alpha * torch.log(twoBlambda)
+	# 		+ (alpha + 0.5) * torch.log(v * (y - gamma) ** 2 + twoBlambda)
+	# 		+ torch.lgamma(alpha)
+	# 		- torch.lgamma(alpha + 0.5)
+	# 	)
+
+	# 	return torch.mean(nll) if reduce else nll
+
+	def NIG_NLL(self, y, gamma, v, alpha, beta, reduce=True):
+		twoBlambda = 2*beta*(1+v)
+
+		nll = 0.5*torch.log(np.pi/v)  \
+			- alpha*torch.log(twoBlambda)  \
+			+ (alpha+0.5) * torch.log(v*(y-gamma)**2 + twoBlambda)  \
+			+ torch.lgamma(alpha)  \
+			- torch.lgamma(alpha+0.5)
 
 		return torch.mean(nll) if reduce else nll
 
+	# def KL_NIG(self, mu1, v1, a1, b1, mu2, v2, a2, b2):
+	# 	KL = (
+	# 		0.5 * (a1 - 1) / b1 * (v2 * torch.square(mu2 - mu1))
+	# 		+ 0.5 * v2 / v1
+	# 		- 0.5 * torch.log(torch.abs(v2) / torch.abs(v1))
+	# 		- 0.5
+	# 		+ a2 * torch.log(b1 / b2)
+	# 		- (torch.lgamma(a1) - torch.lgamma(a2))
+	# 		+ (a1 - a2) * torch.digamma(a1)
+	# 		- (b1 - b2) * a1 / b1
+	# 	)
+	# 	return KL
+
 	def KL_NIG(self, mu1, v1, a1, b1, mu2, v2, a2, b2):
-		KL = (
-			0.5 * (a1 - 1) / b1 * (v2 * torch.square(mu2 - mu1))
-			+ 0.5 * v2 / v1
-			- 0.5 * torch.log(torch.abs(v2) / torch.abs(v1))
-			- 0.5
-			+ a2 * torch.log(b1 / b2)
-			- (torch.lgamma(a1) - torch.lgamma(a2))
-			+ (a1 - a2) * torch.digamma(a1)
-			- (b1 - b2) * a1 / b1
-		)
+		KL = 0.5*(a1-1)/b1 * (v2*torch.square(mu2-mu1))  \
+			+ 0.5*v2/v1  \
+			- 0.5*torch.log(torch.abs(v2)/torch.abs(v1))  \
+			- 0.5 + a2*torch.log(b1/b2)  \
+			- (torch.lgamma(a1) - torch.lgamma(a2))  \
+			+ (a1 - a2)*torch.digamma(a1)  \
+			- (b1 - b2)*a1/b1
+
 		return KL
 
-	def NIG_Reg(
-		self, y, gamma, v, alpha, beta, omega=0.01, reduce=True, kl=False
-	):
-		error = torch.abs(y - gamma)
+	# def NIG_Reg(
+	# 	self, y, gamma, v, alpha, beta, omega=0.01, reduce=True, kl=False
+	# ):
+	# 	error = torch.abs(y - gamma)
+
+	# 	if kl:
+	# 		kl = self.KL_NIG(
+	# 			gamma, v, alpha, beta, gamma, omega, 1 + omega, beta
+	# 		)
+	# 		reg = error * kl
+	# 	else:
+	# 		evi = 2 * v + (alpha)
+	# 		reg = error * evi
+
+	# 	return torch.mean(reg) if reduce else reg
+
+	def NIG_Reg(self, y, gamma, v, alpha, beta, omega=0.01, reduce=True, kl=False):
+		# error = tf.stop_gradient(tf.abs(y-gamma))
+		error = torch.abs(y-gamma)
 
 		if kl:
-			kl = self.KL_NIG(
-				gamma, v, alpha, beta, gamma, omega, 1 + omega, beta
-			)
-			reg = error * kl
+			kl = self.KL_NIG(gamma, v, alpha, beta, gamma, omega, 1+omega, beta)
+			reg = error*kl
 		else:
-			evi = 2 * v + (alpha)
-			reg = error * evi
+			evi = 2*v+(alpha)
+			reg = error*evi
 
 		return torch.mean(reg) if reduce else reg
 
+	# def EvidentialRegression(self, y_true, evidential_output, coeff):
+
+	# 	# print(y_true.shape)
+	# 	# print(evidential_output.shape)
+
+	# 	gamma, v, alpha, beta = torch.split(evidential_output, 1, dim=-1)
+	# 	loss_nll = self.NIG_NLL(y_true, gamma, v, alpha, beta)
+	# 	loss_reg = self.NIG_Reg(y_true, gamma, v, alpha, beta)
+
+	# 	# print("y_true", y_true)
+	# 	# print("evidential_output", evidential_output)
+	# 	# print("gamma", gamma)
+	# 	# print("v", v)
+	# 	# print("alpha", alpha)
+	# 	# print("beta", beta)
+	# 	# print("loss_nll", loss_nll)
+	# 	# print("loss_reg", loss_reg)
+
+	# 	return loss_nll + coeff * loss_reg
+
 	def EvidentialRegression(self, y_true, evidential_output, coeff):
-		gamma, v, alpha, beta = torch.split(y_true, 1, dim=-1)
+		gamma = evidential_output["energy"]
+		v = evidential_output["v"]
+		alpha = evidential_output["alpha"]
+		beta = evidential_output["beta"]
+
+		# print(evidential_output)
+
+		# gamma, v, alpha, beta = torch.split(evidential_output, split_size_or_sections=1, dim=-1)
+		
 		loss_nll = self.NIG_NLL(y_true, gamma, v, alpha, beta)
 		loss_reg = self.NIG_Reg(y_true, gamma, v, alpha, beta)
+
+		# print("y_true.shape", y_true.shape)
+		# print("y_true", y_true)
+		# print("evidential_output", evidential_output)
+		# # print("gamma", gamma)
+		# # print("v", v)
+		# # print("alpha", alpha)
+		# # print("beta", beta)
+		# print("loss_nll", loss_nll)
+		# print("loss_reg", loss_reg)
+
 		return loss_nll + coeff * loss_reg
 
 	def forward(self, input: torch.Tensor, target: torch.Tensor):
-		return self.EvidentialRegression(
-			y_true=input, evidential_output=target, coeff=self.lambda_
-		)
+
+		# print()
+		# loss = self.EvidentialRegression(y_true=input, evidential_output=target, coeff=self.lambda_)
+		loss = self.EvidentialRegression(y_true=target, evidential_output=input, coeff=self.lambda_)
+		# print()
+
+		return loss

ocpmodels/trainers/energy_trainer.py
@@ -103,10 +103,12 @@ class EnergyTrainer(BaseTrainer):
     def load_task(self):
         logging.info(f"Loading dataset: {self.config['task']['dataset']}")
         print(self.config)
-        if self.config["model_attributes"]["evidential"]:
-            self.num_targets = 4
-        else:
-            self.num_targets = 1
+        self.num_targets = 1
+
+        # if self.config["model_attributes"]["evidential"]:
+        #     self.num_targets = 4
+        # else:
+        #     self.num_targets = 1
 
     @torch.no_grad()
     def predict(
@@ -127,13 +129,16 @@ class EnergyTrainer(BaseTrainer):
             loader = [[loader]]
 
         self.model.eval()
+
+        print(self.model)
+
         if self.ema:
             self.ema.store()
             self.ema.copy_to()
 
         if self.normalizers is not None and "target" in self.normalizers:
             self.normalizers["target"].to(self.device)
-        predictions = {"id": [], "energy": []}
+        predictions = {"id": [], "energy": [], "v": [], "alpha": [], "beta": []}
 
         for i, batch in tqdm(
             enumerate(loader),
@@ -145,26 +150,37 @@ class EnergyTrainer(BaseTrainer):
             with torch.cuda.amp.autocast(enabled=self.scaler is not None):
                 out = self._forward(batch)
 
+            # print()
+            # print("output before normalization")
+            # print(out)
+
             if self.normalizers is not None and "target" in self.normalizers:
                 out["energy"] = self.normalizers["target"].denorm(
                     out["energy"]
                 )
+
+            # print("output after normalization")
+            # print(out)
+            # print()
 
             if per_image:
                 predictions["id"].extend(
                     [str(i) for i in batch[0].sid.tolist()]
                 )
                 predictions["energy"].extend(out["energy"].tolist())
+                predictions["v"].extend(out["v"].tolist())
+                predictions["alpha"].extend(out["alpha"].tolist())
+                predictions["beta"].extend(out["beta"].tolist())
             else:
                 predictions["energy"] = out["energy"].detach()
                 return predictions
 
-        self.save_results(predictions, results_file, keys=["energy"])
+        self.save_results(predictions, results_file, keys=["energy", "v", "alpha", "beta"])
 
         if self.ema:
             self.ema.restore()
 
-        return predictions
+        return predictions 
 
     def train(self, disable_eval_tqdm=False):
         eval_every = self.config["optim"].get(
@@ -303,7 +319,10 @@ class EnergyTrainer(BaseTrainer):
             output = output.view(-1)
 
         return {
-            "energy": output,
+            "energy": output[:, 0],
+            "v": output[:, 1],
+            "alpha": output[:, 2],
+            "beta": output[:, 3],
         }
 
     def _compute_loss(self, out, batch_list):
@@ -316,7 +335,8 @@ class EnergyTrainer(BaseTrainer):
         else:
             target_normed = energy_target
 
-        loss = self.loss_fn["energy"](out["energy"], target_normed)
+        # loss = self.loss_fn["energy"](torch.cat(list(out.values()), axis=-1), target_normed)
+        loss = self.loss_fn["energy"](out, target_normed)
         return loss
 
     def _compute_metrics(self, out, batch_list, evaluator, metrics={}):
@@ -324,8 +344,9 @@ class EnergyTrainer(BaseTrainer):
             [batch.y_relaxed.to(self.device) for batch in batch_list], dim=0
         )
 
-        gamma, v, alpha, beta = torch.split(out["energy"], 1, dim=-1)
-        pred = {"energy": gamma}
+        # gamma, v, alpha, beta = torch.split(out["energy"], 1, dim=-1)
+        # pred = {"energy": gamma}
+        pred = {"energy": out["energy"]}
 
         if self.normalizer.get("normalize_labels", False):
             pred["energy"] = self.normalizers["target"].denorm(pred["energy"])